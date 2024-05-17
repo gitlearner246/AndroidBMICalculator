624aa5bb74718fc82a3a81d00493ec5c7772f033
gitlearner246 <yourverygoodfriendaki@gmail.com>
2024-05-17 19:25:27 +0900

    complete

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     // Class Variables; also are called "Fields"
@@ -53,5 +55,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void calculateBmi() {
+        String ageText = ageEditText.getText().toString();
+        String feetText = feetEditText.getText().toString();
+        String inchesText = inchesEditText.getText().toString();
+        String weightText = weightEditText.getText().toString();
+
+        //  converting the number 'StringS' into 'int' variables
+        int age = Integer.parseInt(ageText);
+        int feet = Integer.parseInt(feetText);
+        int inches = Integer.parseInt(inchesText);
+        int weight = Integer.parseInt(weightText);
+
+        int totalInches = (feet * 12) + inches;
+
+        double heightInMeters = totalInches * 0.0254;
+
+        double bmi = weight / (heightInMeters * heightInMeters);
+
+        // convert the decimal/double into string
+        DecimalFormat myDecimalFormatter = new DecimalFormat("0.00");
+        String bmiTextResult = myDecimalFormatter.format(bmi);
+
+        String fullResultString;
+        if (bmi < 18.5) {
+            // display underweight
+            fullResultString = bmiTextResult + " - You are underweight!";
+        } else if (bmi > 25) {
+            // display overweight
+            fullResultString = bmiTextResult + " - You are overweight!";
+        } else {
+            // display healthy
+            fullResultString = bmiTextResult + " - You are a healthy weight!";
+        }
+        resultText.setText(fullResultString);
     }
 }
